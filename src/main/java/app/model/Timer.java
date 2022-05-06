c5c84c78237f0c7d8125f92f60720fd9a4a7df58
test <reshka5776@gmail.com>
2022-05-06 14:51:31 +0300

    Initial commit

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,8 +1,21 @@
 package app.model;
 
+import jdk.jfr.Timespan;
+import jdk.jfr.Timestamp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+
+
+@Component
+
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
+
 
     public Long getTime() {
         return nanoTime;
